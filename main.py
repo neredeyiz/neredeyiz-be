59e55c9b910ff1a14071c53987ee1d8af9f2a9be
neredeyiz <ruffle461@windowslive.com>
2025-03-30 14:17:11 +0300

    Fix time comparison

--- main.py
+++ main.py
@@ -197,6 +197,7 @@ def _set_final_place(db, selected_places):
 
 def _is_within_two_hours_from_now(gathering_time):
     current_datetime = datetime.datetime.now(tz=turkey_tz)
+    gathering_time = turkey_tz.localize(gathering_time)
     logger.info(f"Current time: {current_datetime}, Gathering time: {gathering_time}")
     time_difference = gathering_time - current_datetime
     return time_difference.total_seconds() <= 2 * 60 * 60
